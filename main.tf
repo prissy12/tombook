caba4b99f2a0b25895464866c9df5da132f48978
prissy12 <lastone2cu@yahoo.com>
2017-03-12 17:15:42 -0400

    Add files via upload

--- main.tf
+++ main.tf
@@ -12,6 +12,10 @@ echo "hello world" > index.html
 nohup busybox httpd -f -p "${var.server_port} &
 EOF
 
+lifecycle {
+create_before_destroy = true
+}
+}
 resource "aws_security_group" "instance" {
 name = terraform-example-instance"
 ingress {
@@ -20,7 +24,26 @@ to port = "${var.server_port}"
 protocol = "tcp"
 cidr_blocks = ["0.0.0.0/0"]
 }
+resource "aws_autoscalling_group" "example" {
+launch_configuration = "${aws_launch_configuration.example.id}"
+availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+min_size = 2
+max_size = 10
+tag {
+key = "Name"
+value = "terraform-asg-example"
+propage_at_launch=true
 }
+}
+lifecycle {
+create_before_destroy = true
+}
+}
+tags {
+Name = "terraform-example"
+}
+
 output "public_ip" {
 value = "$[aws_instance.example.public_ip}"
 }
